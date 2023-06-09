d33f905b209034b0bf43f86abea368afc8de03ba
JAHIR RANA <rana78085@gmail.com>
2023-06-09 08:57:49 +0600

    Add head section

--- components/e-commerce.js
+++ components/e-commerce.js
@@ -1,11 +1,17 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Ecommerce = () => {
   return (
-    <div>
-      <h3 className="bg-gray-400 text-center font-bold text-4xl py-7 text-white">E-commerce Home Page</h3>
+    <div className="flex justify-between px-5 items-center py-7 border">
+      <h3 className="min-h-full text-center text-4xl text-blue-500 cursor-pointer hover:scale-110 uppercase">E-commerce</h3>
+      <div className="hover:text-blue-500 font-bold text-3xl cursor-pointer">
+        <Link href="https://www.google.com">
+          <i className="fa-solid fa-cart-shopping"></i>
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default Ecommerce
+export default Ecommerce;
